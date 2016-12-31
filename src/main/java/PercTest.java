62f94bcaeafc3b5633690c31a1ef46f2447f6443
Jack Jackerson <jackjackerson@gmail.com>
2016-12-30 19:44:44 -0500

    added open method logic

--- src/main/java/PercTest.java
+++ src/main/java/PercTest.java
@@ -5,15 +5,16 @@ public class PercTest {
 	public static void main(String[] args) {
 		int n = 5;
 		Percolation perc = new Percolation(n);
-//		System.out.println(Arrays.toString(perc.getParentArray()));
-		System.out.println(Arrays.toString(perc.getStateArray()));
-//		System.out.println(perc.numberOfOpenSites());
-//		for (int i = 1; i <= 5; i++) {
-//			for (int j = 1; j <= 5; j++) {
-//				System.out.print("row: " + i + " col: " + j + " parent val: " + perc.getparentValue(i, j));
-//				System.out.println();
-//			}
-//		}
+//		System.out.println(Arrays.toString(perc.getStateArray()));
+		System.out.println(perc.isOpen(1, 1));
+		perc.open(1, 1);
+		perc.open(1, 2);
+		System.out.println(perc.isOpen(1, 1));
+		System.out.println(perc.numberOfOpenSites());
+		System.out.println(perc.parentArray.connected(0, 1));
+		System.out.println(perc.parentArray.connected(0, 5));
+		perc.open(2, 1);
+		System.out.println(perc.parentArray.connected(0, 5));
 	}
 
 }
--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -2,49 +2,35 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private int openCount = 0;
-	private WeightedQuickUnionUF parentArray;
+	//FIXME make private
+	public WeightedQuickUnionUF parentArray;
 	private boolean[] stateArray;
 	private int dimension;
+	private int columns;
 
 	public Percolation(int n) {
+		if (n <= 0) throw new java.lang.IllegalArgumentException();
 		this.dimension = n;
-		int columns = n * n;
+		this.columns = dimension * dimension;
 		parentArray = new WeightedQuickUnionUF(columns);
 		stateArray = new boolean[columns];
 	}
-
+	
 	public void open(int row, int col) {
+		validateRowCol(row, col);
 		int arrVal = xyTo1D(row, col);
 		stateArray[arrVal] = true;
 		openCount++;
-		
-		// connect to other open neighbors.
-		// recursively?
-	}
-	
-	private int[] getOpenNeighbors(int val) {
-		
-		int[] neighbors = new int[4];
-		//above
-		if (val - dimension >= 0) neighbors[0] = val - dimension;
-		//below
-		if (val + dimension < dimension*dimension) neighbors[1] = val + dimension;
-		//left
-		
-		//right
-		
-		int left;
-		int right;
-		int below;
-		
-		return null;
+		unionNeighbors(arrVal);
 	}
 	
 	public boolean isOpen(int row, int col) {
+		validateRowCol(row, col);
 		return stateArray[xyTo1D(row, col)];
 	}
-
+	
 	public boolean isFull(int row, int col) {
+		validateRowCol(row, col);
 		// is site (row, col) full?
 		// isOpen(row, col)
 		return false;
@@ -62,8 +48,25 @@ public class Percolation {
 	private int xyTo1D(int row, int col) {
 		return (dimension * (row - 1)) + col - 1;
 	}
-
-	public boolean[] getStateArray() {
-		return stateArray;
+	
+	private void unionNeighbors(int val) {
+		if (val - dimension >= 0 && stateArray[val - dimension]) { //above
+			parentArray.union(val, val - dimension);
+		} 
+		if (val + dimension < columns && stateArray[val + dimension]) { //below
+			parentArray.union(val, val + dimension);
+		}
+		if (val % dimension > 0 && stateArray[val - 1]) { //left
+			parentArray.union(val, val - 1);
+		}
+		if (val % dimension < 4 && stateArray[val + 1]) { //right
+			parentArray.union(val, val + 1);
+		}
+	}
+	
+	private void validateRowCol(int row, int col){
+		if (row <= 0 || row > dimension || col <= 0 || col > dimension){
+			throw new java.lang.IndexOutOfBoundsException();
+		}
 	}
 }
