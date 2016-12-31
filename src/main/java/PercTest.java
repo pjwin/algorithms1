1f7aeef20a600684e9b06a97cdd109a09ec64253
PJ Winters <winterspj@gmail.com>
2016-12-31 12:07:32 -0500

    its mostly working
    Signed-off-by: PJ Winters <winterspj@gmail.com>

--- src/main/java/PercTest.java
+++ src/main/java/PercTest.java
@@ -5,16 +5,17 @@ public class PercTest {
 	public static void main(String[] args) {
 		int n = 5;
 		Percolation perc = new Percolation(n);
-//		System.out.println(Arrays.toString(perc.getStateArray()));
 		System.out.println(perc.isOpen(1, 1));
-		perc.open(1, 1);
 		perc.open(1, 2);
-		System.out.println(perc.isOpen(1, 1));
-		System.out.println(perc.numberOfOpenSites());
-		System.out.println(perc.parentArray.connected(0, 1));
-		System.out.println(perc.parentArray.connected(0, 5));
 		perc.open(2, 1);
-		System.out.println(perc.parentArray.connected(0, 5));
+		perc.open(2, 2);
+		perc.open(2, 3);
+		perc.open(3, 2);
+		System.out.println(perc.numberOfOpenSites());
+		System.out.println(perc.parentArray.connected(6, 1));
+		System.out.println(perc.parentArray.connected(6, 5));
+		System.out.println(perc.parentArray.connected(6, 7));
+		System.out.println(perc.parentArray.connected(6, 11));
 	}
 
 }
--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -2,38 +2,39 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private int openCount = 0;
-	//FIXME make private
+	// FIXME make private
 	public WeightedQuickUnionUF parentArray;
 	private boolean[] stateArray;
 	private int dimension;
 	private int columns;
 
 	public Percolation(int n) {
-		if (n <= 0) throw new java.lang.IllegalArgumentException();
+		if (n <= 0)	throw new java.lang.IllegalArgumentException();
 		dimension = n;
 		columns = dimension * dimension;
 		parentArray = new WeightedQuickUnionUF(columns);
 		stateArray = new boolean[columns];
 	}
-	
+
 	public void open(int row, int col) {
 		validateRowCol(row, col);
 		int arrVal = xyTo1D(row, col);
 		stateArray[arrVal] = true;
 		openCount++;
-		unionNeighbors(arrVal);
+		findNeighbors(arrVal);
 	}
-	
+
 	public boolean isOpen(int row, int col) {
 		validateRowCol(row, col);
 		return stateArray[xyTo1D(row, col)];
 	}
-	
+
 	public boolean isFull(int row, int col) {
 		validateRowCol(row, col);
 		if (isOpen(row, col)) {
 			for (int i = 0; i < dimension; i++) {
-				if (parentArray.connected(xyTo1D(row, col), i)) return true;
+				if (parentArray.connected(xyTo1D(row, col), i))
+					return true;
 			}
 		}
 		return false;
@@ -51,29 +52,41 @@ public class Percolation {
 	private int xyTo1D(int row, int col) {
 		return (dimension * (row - 1)) + col - 1;
 	}
-	
-	private void unionNeighbors(int val) {
-		if (val - dimension >= 0 && stateArray[val - dimension]) { //above
-			parentArray.union(val, val - dimension);
-		} 
-		if (val + dimension < columns && stateArray[val + dimension]) { //below
-			parentArray.union(val, val + dimension);
+
+	private void findNeighbors(int val) {
+		int neighbor;
+		neighbor = val - dimension; // above
+		if (neighbor >= 0 && stateArray[neighbor]) {
+			unionNeighbor(val, neighbor);
 		}
-		if (val % dimension > 0 && stateArray[val - 1]) { //left
-			parentArray.union(val, val - 1);
+		neighbor = val + dimension; // below
+		if (neighbor < columns && stateArray[neighbor]) {
+			unionNeighbor(val, neighbor);
 		}
-		if (val % dimension < 4 && stateArray[val + 1]) { //right
-			parentArray.union(val, val + 1);
+		neighbor = val - 1; // left
+		if ((val % dimension > 0) && (neighbor >= 0) && stateArray[neighbor]) {
+			unionNeighbor(val, neighbor);
+		}
+		neighbor = val + 1; // right
+		if ((val % dimension < (dimension - 1)) && (neighbor < columns) && stateArray[neighbor]) {
+			unionNeighbor(val, neighbor);
+		}
+	}
+
+	private void unionNeighbor(int val, int neighbor) {
+		if (!(parentArray.connected(val, neighbor))) {
+			parentArray.union(val, neighbor);
+			findNeighbors(neighbor);
 		}
 	}
 	
-	private void validateRowCol(int row, int col){
+	private void validateRowCol(int row, int col) {
 		if (row <= 0 || row > dimension) {
 			throw new java.lang.IndexOutOfBoundsException("row index out of bounds");
 		}
-	    if (col <= 0 || col > dimension) {
-	    	throw new java.lang.IndexOutOfBoundsException("col index out of bounds");	    	
-	    }
-	    	
+		if (col <= 0 || col > dimension) {
+			throw new java.lang.IndexOutOfBoundsException("col index out of bounds");
+		}
+
 	}
 }
