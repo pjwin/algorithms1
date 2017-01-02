d8dc67dddb53217b3b1cd2ef6bf08c11dc1c4107
PJ Winters <winterspj@gmail.com>
2017-01-02 11:57:44 -0500

    fixing backwash
    Signed-off-by: PJ Winters <winterspj@gmail.com>

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,95 +1,107 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private int openCount = 0;
-	private WeightedQuickUnionUF parentArray;
-	private boolean[] stateArray;
-	private int dimension;
-	private int columns;
-	
-	public int union = 0;
+    private int openCount = 0;
+    private WeightedQuickUnionUF parentArray;
+    private boolean[] stateArray;
+    private boolean[] topArray;
+    private boolean[] bottomArray;
+    private int dimension;
+    private int columns;
+    private boolean percolates = false;
 
-	public Percolation(int n) {
-		if (n <= 0)	throw new java.lang.IllegalArgumentException();
-		dimension = n;
-		columns = dimension * dimension;
-		parentArray = new WeightedQuickUnionUF(columns + 2);
-		stateArray = new boolean[columns];
-	}
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new java.lang.IllegalArgumentException();
+        dimension = n;
+        columns = dimension * dimension;
+        parentArray = new WeightedQuickUnionUF(columns);
+        stateArray = new boolean[columns];
+        topArray = new boolean[columns];
+        bottomArray = new boolean[columns];
+    }
 
-	public void open(int row, int col) {
-		validateRowCol(row, col);
-		int arrVal = xyTo1D(row, col);
-		stateArray[arrVal] = true;
-		openCount++;
-		findNeighbors(arrVal);
-	}
+    public void open(int row, int col) {
+        validateRowCol(row, col);
+        int arrVal = xyTo1D(row, col);
+        if (!(stateArray[arrVal])) {
+            stateArray[arrVal] = true;
+            openCount++;
+            findNeighbors(arrVal);
+        }
+    }
 
-	public boolean isOpen(int row, int col) {
-		validateRowCol(row, col);
-		return stateArray[xyTo1D(row, col)];
-	}
+    private void findNeighbors(int val) {
+        // connect to "top"
+        if (val < dimension) {
+            topArray[val] = true;
+        }
+        // connect to "bottom"
+        if (((columns - dimension) < val) && (val < columns)) {
+            bottomArray[parentArray.find(val)] = true;
+        }
+    
+        int neighbor;
+        neighbor = val - dimension; // above
+        if (neighbor >= 0 && stateArray[neighbor])
+            unionNeighbor(val, neighbor);
+    
+        neighbor = val + dimension; // below
+        if (neighbor < columns && stateArray[neighbor])
+            unionNeighbor(val, neighbor);
+    
+        neighbor = val - 1; // left
+        if ((val % dimension > 0) && (neighbor >= 0) && stateArray[neighbor])
+            unionNeighbor(val, neighbor);
+    
+        neighbor = val + 1; // right
+        if ((val % dimension < (dimension - 1)) && (neighbor < columns) && stateArray[neighbor])
+            unionNeighbor(val, neighbor);
+    }
 
-	public boolean isFull(int row, int col) {
-		validateRowCol(row, col);
-		if (isOpen(row, col) && parentArray.connected(xyTo1D(row, col), columns)) {
-			return true;
-		}
-		return false;
-	}
+    public boolean isFull(int row, int col) {
+        validateRowCol(row, col);
+        if (isOpen(row, col) && topArray[xyTo1D(row, col)]) {
+            return true;
+        }
+        return false;
+    }
 
-	public int numberOfOpenSites() {
-		return openCount;
-	}
+    public boolean percolates() {
+        return percolates;
+    }
 
-	public boolean percolates() {
-		if (parentArray.connected(columns, columns + 1)) {
-			return true;
-		}
-		return false;
-	}
+    private void unionNeighbor(int val, int neighbor) {
+        if (topArray[neighbor]) {
+            topArray[val] = true;
+        }
+        if (topArray[val] == true && topArray[neighbor] == false) {
+            topArray[neighbor] = true;
+        }
+        if (!(parentArray.connected(val, neighbor))) {
+            parentArray.union(parentArray.find(val), parentArray.find(neighbor));
+        }
+    }
 
-	private int xyTo1D(int row, int col) {
-		return (dimension * (row - 1)) + col - 1;
-	}
+    public int numberOfOpenSites() {
+        return openCount;
+    }
 
-	private void findNeighbors(int val) {
-		int neighbor;
-		//if on the top row, connect to "top" element n*n
-		if (val < dimension) parentArray.union(parentArray.find(val), parentArray.find(columns));
-		//if on the bottom row, connect to "bottom" element n*n + 1
-		if (((columns - dimension) < val) && (val < columns)) parentArray.union(parentArray.find(val), parentArray.find(columns + 1));
-		
-		neighbor = val - dimension; // above
-		if (neighbor >= 0 && stateArray[neighbor]) {
-			unionNeighbor(val, neighbor);
-		}
-		neighbor = val + dimension; // below
-		if (neighbor < columns && stateArray[neighbor]) {
-			unionNeighbor(val, neighbor);
-		}
-		neighbor = val - 1; // left
-		if ((val % dimension > 0) && (neighbor >= 0) && stateArray[neighbor]) {
-			unionNeighbor(val, neighbor);
-		}
-		neighbor = val + 1; // right
-		if ((val % dimension < (dimension - 1)) && (neighbor < columns) && stateArray[neighbor]) {
-			unionNeighbor(val, neighbor);
-		}
-	}
+    public boolean isOpen(int row, int col) {
+        validateRowCol(row, col);
+        return stateArray[xyTo1D(row, col)];
+    }
 
-	private void unionNeighbor(int val, int neighbor) {
-		if (!(parentArray.connected(val, neighbor))) {
-			parentArray.union(parentArray.find(val), parentArray.find(neighbor));		
-		}
-	}
-	
-	private void validateRowCol(int row, int col) {
-		if (row <= 0 || row > dimension) {
-			throw new java.lang.IndexOutOfBoundsException("row index out of bounds");
-		}
-		if (col <= 0 || col > dimension) {
-			throw new java.lang.IndexOutOfBoundsException("col index out of bounds");
-		}
-	}
+    private int xyTo1D(int row, int col) {
+        return (dimension * (row - 1)) + col - 1;
+    }
+
+    private void validateRowCol(int row, int col) {
+        if (row <= 0 || row > dimension) {
+            throw new java.lang.IndexOutOfBoundsException("row index out of bounds:" + row);
+        }
+        if (col <= 0 || col > dimension) {
+            throw new java.lang.IndexOutOfBoundsException("col index out of bounds:" + col);
+        }
+    }
 }
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,33 +1,47 @@
-import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
-	public PercolationStats(int n, int trials) {
-		// perform trials independent experiments on an n-by-n grid
-	}
+    private double[] values;
+    private int trials;
 
-	public double mean() {
-		return 0; 
-		// sample mean of percolation threshold
-	}
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new java.lang.IllegalArgumentException();
+        this.values = new double[trials];
+        this.trials = trials;
+        double sites = n * n;
 
-	public double stddev() {
-		return 0;
-		// sample standard deviation of percolation threshold
-	}
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!(perc.percolates())) {
+                // FIXME generate a unique pair not chosen yet.
+                perc.open(StdRandom.uniform(n) + 1, StdRandom.uniform(n) + 1);
+            }
+            this.values[i] = ((double) perc.numberOfOpenSites() / sites);
+        }
+    }
 
-	public double confidenceLo() {
-		return 0;
-		// low endpoint of 95% confidence interval
-	}
+    public double mean() {
+        return StdStats.mean(values);
+    }
 
-	public double confidenceHi() {
-		return 0;
-		// high endpoint of 95% confidence interval
-	}
+    public double stddev() {
+        return StdStats.stddev(values);
+    }
 
-	public static void main(String[] args) {
-		// test client (described below)
-	}
+    public double confidenceLo() {
+        return StdStats.mean(values) - (1.96 / Math.sqrt(trials));
+    }
+
+    public double confidenceHi() {
+        return StdStats.mean(values) + (1.96 / Math.sqrt(trials));
+    }
+
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(200, 100);
+        System.out.println("mean: " + ps.mean());
+        System.out.println("stddev: " + ps.stddev());
+        System.out.println("95% confidence interval: " + ps.confidenceLo() + ", " + ps.confidenceHi());
+    }
 }
